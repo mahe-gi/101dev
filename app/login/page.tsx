e35d8c1328f7553c805640e9330ab2c171a339b1
mahesh ch <chmahesh.dev@gmail.com>
2025-04-15 00:17:42 +0530

    auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,18 @@
-export default function LoginPage() {
+import Appbar from "@/components/Appbar";
+import { getServerSession } from "next-auth";
+
+export default async function LoginPage() {
+  const session = await getServerSession();
+
   return (
     <div>
       <h1>Login Page</h1>
       <form>
-        <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
+        <Appbar />
+
+        <div>user component -server : {JSON.stringify(session)}</div>
       </form>
     </div>
   );
--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
 export default function Appbar() {
+  const session = useSession();
   return (
     <header className="">
       <h1>Logo</h1>
+      <button onClick={() => signIn()}>Signin</button>
+      <button onClick={() => signOut()}>Sign out</button>
+
+      <div>{JSON.stringify(session)}</div>
     </header>
   );
 }
