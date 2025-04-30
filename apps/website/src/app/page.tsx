73d62471c5d822fcf576c82de1078588e7e1b499
mahesh ch <chmahesh.dev@gmail.com>
2025-05-01 00:18:39 +0530

    a

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,24 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className=" h-screen w-screen flex items-center justify-center">
-      <div>
-        <h1 className=" font-medium text-[4vw]">
-          ➡️{" "}
-          <span className=" text-green-600">
-            {" "}
-            <Link
-              href={
-                "https://massive-dodo-f8e.notion.site/MANOJ-SIR-CLASS-1e576887e01a80bbb228f33850ae2b74"
-              }
-            >
-              {" "}
-              CLICK HERE TO OPEN DOC !
-            </Link>
-          </span>
-        </h1>
-      </div>
-    </div>
-  );
+  return <div>hello</div>;
 }
